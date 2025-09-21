82531e7d698236f5215e63104ff8f1759dbfbdf5
Hafiz Mudassir Husain <hmudassir511@gmail.com>
2025-09-21 11:47:20 -0700

    updated pkg json

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -14,12 +14,12 @@ export const AuthProvider = ({ children }) => {
     const storedToken = localStorage.getItem("token");
     const storedRole = localStorage.getItem("role");
     const storedName = localStorage.getItem("name");
-  
+
     if (storedToken && storedRole) {
       setUser({ name: storedName, role: storedRole });
     }
   }, []);
-  
+
 
   // ✅ Load user from localStorage on first render
   const [user, setUser] = useState(() => {
@@ -33,9 +33,9 @@ export const AuthProvider = ({ children }) => {
       const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, { email, password });
       const loggedInUser = { name: response.data.name, role: response.data.role };
 
-     localStorage.setItem("token", response.data.token);
-localStorage.setItem("user", JSON.stringify({ name: response.data.name, role: response.data.role, userId: response.data.userId })); // ✅ Store full user object
-console.log("✅ Login Success - Full Response:", response.data);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify({ name: response.data.name, role: response.data.role, userId: response.data.userId })); // ✅ Store full user object
+      console.log("✅ Login Success - Full Response:", response.data);
 
       setUser(loggedInUser);
     } catch (error) {
@@ -48,7 +48,7 @@ console.log("✅ Login Success - Full Response:", response.data);
     localStorage.removeItem("user"); // ✅ Remove full user object
     navigate("/") // ✅ Redirect to login after logout
   };
-  
+
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
